bb1bc4a345dc39c1dc70adc1eb86af6bb9f9aafe
Szymon P <szymon_jozef@proton.me>
2026-04-02 20:40:31 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# LAN game of battleships made with pygame
+
+## Authors:
+- [Szymon P](https://github.com/szymon-jozef)
---
+++ flake.nix
@@ -0,0 +1,116 @@
+{
+  description = "LAN Game of battleships, made with pygame.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      editableOverlay = workspace.mkEditablePyprojectOverlay {
+        root = "$REPO_ROOT";
+      };
+
+      pythonSets = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+        in
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.wheel
+              overlay
+            ]
+          )
+      );
+
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pythonSet = pythonSets.${system}.overrideScope editableOverlay;
+          virtualenv = pythonSet.mkVirtualEnv "battleships-pygame-dev-env" workspace.deps.all;
+          runtimeLibs = with pkgs; [
+            libGL
+            libxkbcommon
+            wayland
+            wayland-protocols
+            libdecor
+
+            libX11
+            libXcursor
+            libXext
+            libXinerama
+            libXi
+            libXrandr
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+              pkgs.ruff
+            ]
+            ++ runtimeLibs;
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = pythonSet.python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+
+              SDL_VIDEODRIVER = "wayland,x11";
+
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+            };
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
+          };
+        }
+      );
+      packages = forAllSystems (system: {
+        default = pythonSets.${system}.mkVirtualEnv "battleships-pygame-env" workspace.deps.default;
+      });
+    };
+}
