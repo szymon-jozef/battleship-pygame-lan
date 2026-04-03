47348757721d2f5511c48a839376dd36c2354bc4
Szymon P <szymon_jozef@proton.me>
2026-04-03 20:03:47 +0200

    feat: update main with new functionality showcase

--- src/battleship_pygame_lan/__main__.py
+++ src/battleship_pygame_lan/__main__.py
@@ -1,6 +1,6 @@
 import logging
 
-from battleship_pygame_lan.logic import Board, ShipType
+from battleship_pygame_lan.logic import Player, ShipType
 
 
 def main() -> None:
@@ -8,14 +8,28 @@ def main() -> None:
     logging.basicConfig(filename="battleships.log", level=logging.INFO)
     logger.info("Started")
 
-    # dirty code test
-    board = Board()
-    board.place_ship(ShipType.OneMaster, 1, 1)
-    board.place_ship(ShipType.FourMaster, 5, 5, False)
-    print(str(board))
-    board.shoot(1, 1)
-    print(str(board))
-    print(f"Is game over? {board.is_game_over()}")
+    # dirty code showcase
+
+    # create player
+    player1 = Player("Morbius")
+    player1.place_ship(ShipType.ThreeMaster, 1, 1)
+    player1.place_ship(ShipType.FourMaster, 5, 5, False)
+
+    # do stuff with his board
+    print(f"{player1.name} board:")
+    print(player1.board)
+    print(f"{player1.name} radar:")
+    print(player1.radar)
+    print(f"{player1.name} gets shot, but it was a miss!")
+    player1.receive_shot(2, 2)
+    print(player1.board)
+    print(f"{player1.name} gets shot!")
+    player1.receive_shot(1, 1)
+    print(player1.board)
+    print(f"{player1.name} gets sink!")
+    player1.receive_shot(1, 2)
+    player1.receive_shot(1, 3)
+    print(player1.board)
 
     logger.info("Finished")
 
