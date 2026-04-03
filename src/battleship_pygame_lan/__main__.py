da4d8e7ef79a51f9309a005b73f81a7c1101d8a7
Szymon P <szymon_jozef@proton.me>
2026-04-03 14:06:15 +0200

    add some stuff to main

--- src/battleship_pygame_lan/__main__.py
+++ src/battleship_pygame_lan/__main__.py
@@ -1,5 +1,22 @@
+import logging
+
+from battleship_pygame_lan.logic import Board, ShipType
+
+
 def main() -> None:
-    print("Hello from battleship-pygame-lan!")
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename="battleships.log", level=logging.INFO)
+    logger.info("Started")
+
+    # dirty code test
+    board = Board()
+    board.place_ship(ShipType.OneMaster, 1, 1)
+    print(str(board))
+    board.shoot(1, 1)
+    print(str(board))
+    print(f"Is game over? {board.is_game_over()}")
+
+    logger.info("Finished")
 
 
 if __name__ == "__main__":
