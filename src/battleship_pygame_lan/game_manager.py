e2df977e11b85595633ed1fa06e3fa3579b52ed9
Szymon P <szymon_jozef@proton.me>
2026-04-08 14:29:44 +0200

    feat: handle_shot and connect method

--- src/battleship_pygame_lan/game_manager.py
+++ src/battleship_pygame_lan/game_manager.py
@@ -5,6 +5,7 @@ from queue import Empty, Queue
 
 from battleship_pygame_lan.logic import (
     AlreadyShotError,
+    FieldState,
     OutOfBoundsError,
     Player,
     ShotResult,
@@ -39,8 +40,10 @@ class GameManager:
     ) -> None:
         self.player: Player = Player(player_name)
         self.network_client: NetworkClient = NetworkClient(player_name, server_ip)
-        self.network_client.connect()
         self.gui_events_queue: Queue[GuiEvent] = Queue()
+
+    def connect(self) -> None:
+        self.network_client.connect()
 
     def shoot(self, row: int, column: int) -> None:
         self.network_client.send_attack_info(row, column)
@@ -59,26 +62,74 @@ class GameManager:
             message_type: PayloadTypes = PayloadTypes(message.get("type"))
 
             match message_type:
+                case PayloadTypes.ATTACK:
+                    self._handle_shot(message)
                 case PayloadTypes.SHOT_RESULT:
-                    row: int = int(message.get("row"))
-                    column: int = int(message.get("column"))
-                    shot_result: ShotResult = ShotResult(message.get("result"))
-                    self._handle_shot_result(shot_result)
-                    try:
-                        self.player.mark_shot(row, column, shot_result)
-                    except OutOfBoundsError:
-                        logger.info(
-                            "[GameClient] Enemy reported the shot was out of bounds!"
-                        )
-                    except AlreadyShotError:
-                        logger.info(
-                            "[GameClient] Enemy reported that the player already "
-                            f"made shot at {row, column}"
-                        )
+                    self._handle_shot_result(message)
                 case _:  # we pass for now
                     pass
 
-    def _handle_shot_result(self, shot_result: ShotResult) -> None:
+    def _handle_shot(self, message: dict) -> None:
+        row_content = message.get("row")
+        column_content = message.get("column")
+        if row_content is not None and column_content is not None:
+            row: int = int(row_content)
+            column: int = int(column_content)
+        else:
+            logger.error("[GameClient] row or column is empty in handle_shot()")
+            # if message is weird then we just ignore it
+            return
+
+        field_state: FieldState = self.player.get_own_board_state(row, column)
+        try:
+            self.player.board.shoot(row, column)
+        except OutOfBoundsError:
+            logger.info("[GameClient] Enemy tried to shot out of bounds!")
+            self.network_client.send_shot_result(
+                row, column, ShotResult.AlreadyShot
+            )  # we don't have specific value for that
+            # TODO we probably should do something about that
+            return
+        except AlreadyShotError:
+            logger.info(
+                "[Gameclient] Enemy tried to shot at place that was already shot!"
+            )
+            self.network_client.send_shot_result(row, column, ShotResult.AlreadyShot)
+            return
+
+        if field_state == FieldState.Taken:
+            self.network_client.send_shot_result(row, column, ShotResult.Hit)
+        else:
+            self.network_client.send_shot_result(row, column, ShotResult.Miss)
+
+    def _handle_shot_result(self, message: dict) -> None:
+        row_content = message.get("row")
+        column_content = message.get("column")
+        if row_content is not None and column_content is not None:
+            row: int = int(row_content)
+            column: int = int(column_content)
+        else:
+            logger.error("[GameClient] row or column is empty in handle_shot()")
+            # if message is weird then we just ignore it
+            return
+
+        try:
+            shot_result: ShotResult = ShotResult(message.get("result"))
+        except ValueError:
+            logger.info("[GameClient] weird key in shot_result in handle_shot_result()")
+            return
+        try:
+            self.player.mark_shot(row, column, shot_result)
+        except OutOfBoundsError:
+            logger.info("[GameClient] Enemy reported the shot was out of bounds!")
+            return
+        except AlreadyShotError:
+            logger.info(
+                "[GameClient] Enemy reported that the player already "
+                f"made shot at {row, column}"
+            )
+            return
+
         match shot_result:
             case ShotResult.Hit:
                 self.gui_events_queue.put(GuiEvent.ShotHit)
